fedaa0086c4b8f4b4862125cf95491cd3bf7344b
Dave Briccetti <daveb@davebsoft.com>
2021-01-10 18:22:00 -0800

    Rename hand to deck, to correct the terminology

--- Day22.py
+++ Day22.py
@@ -9,7 +9,7 @@ winner, loser = 0, 0  # To avoid warning
 
 while all(decks):
     round_number += 1
-    drawn_cards: list[int] = [hand.popleft() for hand in decks]
+    drawn_cards: list[int] = [deck.popleft() for deck in decks]
     print(f'Round {round_number}: cards: {drawn_cards}, decks: {decks}')
     winner, loser = (0, 1) if drawn_cards[0] > drawn_cards[1] else (1, 0)
     for i in winner, loser:
