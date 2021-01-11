88f3d3c4e965ef4f6bee561b2b4ef76bd6d83524
Dave Briccetti <daveb@davebsoft.com>
2021-01-10 16:39:29 -0800

    Improve Day 22 code

--- Day22.py
+++ Day22.py
@@ -1,17 +1,18 @@
 from collections import deque
 
-with open('data/advent2020/input22.txt') as file:
-    players = [player.split('\n') for player in file.read().strip().split('\n\n')]
-
+Deck = deque[int]
+with open('data/advent2020/input22_test.txt') as file:
+    player_input: list[str] = file.read().strip().split('\n\n')
+decks: list[Deck] = [deque(map(int, player.split('\n')[1:])) for player in player_input]
 round_number = 0
-hands = [deque(map(int, player[1:])) for player in players]
-while all(hands):
+winner, loser = 0, 0  # To avoid warning
+
+while all(decks):
     round_number += 1
-    drawn_cards: list[int] = [hand.popleft() for hand in hands]
-    print(f'Round {round_number}: cards: {drawn_cards}, hands: {hands}')
+    drawn_cards: list[int] = [hand.popleft() for hand in decks]
+    print(f'Round {round_number}: cards: {drawn_cards}, decks: {decks}')
     winner, loser = (0, 1) if drawn_cards[0] > drawn_cards[1] else (1, 0)
     for i in winner, loser:
-        hands[winner].append(drawn_cards[i])
+        decks[winner].append(drawn_cards[i])
 print(f'Player {winner + 1} wins')
-card_values: list[int] = [(i + 1) * card for i, card in enumerate(reversed(hands[winner]))]
-print(sum(card_values))
+print(sum([(i + 1) * card for i, card in enumerate(reversed(decks[winner]))]))
